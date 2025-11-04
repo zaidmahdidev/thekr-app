0b9170e95afa9f77bfddd82c7faf0106259fe734
Zaid Mahdi <zaidmhdi33@gmail.com>
2025-11-04 03:39:39 +0300

    feat: Upgrade rive and other dependencies
    This commit upgrades the rive dependency from `rive_common` to `rive_native`. This required changes to the plugin registration files for Linux, Windows, and macOS.
    
    Additionally, the following dependencies have been updated:
    - `fluttertoast` to `^9.0.0`
    - Other packages updated in `pubspec.lock`
    
    The build number has been incremented to `2`.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,12 +7,12 @@ import Foundation
 
 import geolocator_apple
 import path_provider_foundation
-import rive_common
+import rive_native
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
+  RiveNativePlugin.register(with: registry.registrar(forPlugin: "RiveNativePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,11 +7,11 @@
 #include "generated_plugin_registrant.h"
 
 #include <geolocator_windows/geolocator_windows.h>
-#include <rive_common/rive_plugin.h>
+#include <rive_native/rive_native_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   GeolocatorWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("GeolocatorWindows"));
-  RivePluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("RivePlugin"));
+  RiveNativePluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("RiveNativePlugin"));
 }
