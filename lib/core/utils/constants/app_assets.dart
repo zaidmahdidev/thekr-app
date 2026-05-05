49ac5dcc82bd3ca61765fda0959a3624b711c570
Zaid Mahdi <zaidmhdi33@gmail.com>
2026-05-05 22:08:52 +0300

    Refactor code structure for improved readability and maintainability

--- lib/core/utils/constants/app_assets.dart
+++ lib/core/utils/constants/app_assets.dart
@@ -5,7 +5,7 @@ class AppAssets {
   static const String _imagesPath = 'assets/images';
 
   // Branding
-  static const String logo = '$_imagesPath/thekr.png';
+  static const String logo = '$_imagesPath/logo.png';
   // static const String quranLogo = '$_imagesPath/quran_logo.png';
   static const String hero = '$_imagesPath/hero.png';
   static const String bg = '$_imagesPath/bg.png';
